6c625bb1442bc94b960fe1694ac0977d379973cc
rayoz6546 <rah194@pitt.edu>
2024-12-04 10:12:02 -0500

    a6 changes

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,31 +1,3 @@
-import Database from "../Database/index.js";
-// import model from "./model.js";
-
-// export function enrollUserInCourse(userId, courseId) {
-//   const { enrollments } = Database;
-//   const newEnroll = { _id: Date.now(), user: userId, course: courseId }
-//   // enrollments.push({ _id: Date.now(), user: userId, course: courseId });
-//   Database.enrollments = [...Database.enrollments, newEnroll]
-//   return enrollments
-
-// }
-
-
-// export function unEnrollUserFromCourse(userId, courseId) {
-
-//   const { enrollments } = Database;
-    
-//   const index = enrollments.findIndex(
-//     (enrollment) => enrollment.course === courseId && enrollment.user === userId
-//   );
-
-//   if (index === -1) {
-//     return false;  // Enrollment not found
-//   }
-
-//   enrollments.splice(index, 1);
-//   return true;
-// }
 import model from "./model.js";
 export async function findCoursesForUser(userId) {
   const enrollments = await model.find({ user: userId }).populate("course");
@@ -41,4 +13,8 @@ return model.create({ user, course });
 }
 export function unEnrollUserFromCourse(user, course) {
 return model.deleteOne({ user, course });
+}
+
+export function unEnrollAll(courseId) { 
+  return model.deleteMany({course:courseId});
 }
